4953ae6db0231f9fe4b92dac5d19a68c329cbd0a
dmostafiz <dev.mostafiz@gmail.com>
2025-08-14 21:19:08 +0600

    okay

---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="bg-gray-800 w-full h-screen !p-8">
+        <div class="relative w-[300px]">
+            <div class="relative">
+                <img class="w-[50%]" src="/img/logo.png" />
+            </div>
+            <div class="absolute top-[18%] opacity-80 left-15">
+                <img class="w-[90%]" src="/img/sign.png" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
