9867f4bdadc745d0fdae92e8bf58d5a29a1a9046
dmostafiz <dev.mostafiz@gmail.com>
2025-08-15 00:26:00 +0600

    okay

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,34 +1,42 @@
 <template>
-    <div class="bg-gray-800 !text-white w-full min-h-screen !p-8 !pb-20">
-        <div class="h-full w-full flex flex-col gap-20 items-center justify-between">
-            <div class="relative md:w-[30%]">
-                <div class="relative">
-                    <img class="w-[50%]" src="/img/logo.png" />
-                </div>
-                <div class="absolute top-[18%] left-15">
-                    <img class="w-[90%]" src="/img/sign.png" />
-                </div>
-            </div>
-
-            <div class="md:w-[50%] text-center text-3xl tracking-widest font-thin leading-10">
-                A Place of Style and Quality.
-                We are more than just a brand; we are your first choice for high-end leather goods, classic apparel, and
-                sophisticated home décor.
-                Each item is crafted to combine comfort, character, and craftsmanship.
-            </div>
-
-            <div class=" w-[300px] flex flex-col items-center justify-center gap-2">
-                <p class="mb-5 text-xl text-center">Proudly Presented by </p>
-                <a target="_blank" href="https://www.bishaloy.com">
-                    <div class=" flex flex-col items-center justify-center gap-3">
-                        <div class="relative">
-                            <img class="w-[200px]" src="https://www.bishaloy.com/logo/logo-main.png" />
+    <div class="bg-gray-900 !text-white w-full min-h-screen">
+        <div class="bg-[url(/img/logo.png)]  bg-cover">
+            <div class="bg-gray-500/20 backdrop-blur-md !p-8 !pb-20">
+                <div class="h-full w-full flex flex-col gap-20 items-center justify-between">
+                    <div class="w-full flex items-center justify-center p-5">
+                        <div class="relative md:w-[20%]">
+                            <div class="relative">
+                                <img class="w-[50%]" src="/img/logo.png" />
+                            </div>
+                            <div class="absolute top-[18%] left-15">
+                                <img class="w-[90%]" src="/img/sign.png" />
+                            </div>
                         </div>
-                        <span class="text-xl tracking-widest">www.bishaloy.com</span>
                     </div>
-                </a>
-            </div>
 
+                    <div class="md:w-[50%] text-center text-3xl tracking-widest font-thin leading-10">
+                        A Place of Style and Quality.
+                        We are more than just a brand; we are your first choice for high-end leather goods, classic
+                        apparel,
+                        and
+                        sophisticated home décor.
+                        Each item is crafted to combine comfort, character, and craftsmanship.
+                    </div>
+
+                    <div class=" w-[300px] flex flex-col items-center justify-center gap-2">
+                        <p class="mb-5 text-xl text-center">Proudly Presented by </p>
+                        <a target="_blank" href="https://www.bishaloy.com">
+                            <div class=" flex flex-col items-center justify-center gap-3">
+                                <div class="relative">
+                                    <img class="w-[200px]" src="https://www.bishaloy.com/logo/logo-main.png" />
+                                </div>
+                                <span class="text-xl tracking-widest">www.bishaloy.com</span>
+                            </div>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
         </div>
     </div>
 </template>
