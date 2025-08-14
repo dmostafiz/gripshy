1acb796d52a753e5b84cbe8813467334a4799817
dmostafiz <dev.mostafiz@gmail.com>
2025-08-14 21:38:35 +0600

    okay

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,34 @@
 <template>
     <div class="bg-gray-800 w-full h-screen !p-8">
-        <div class="relative w-[300px]">
-            <div class="relative">
-                <img class="w-[50%]" src="/img/logo.png" />
+        <div class="h-full w-full flex flex-col gap-20 items-center justify-between">
+            <div class="relative md:w-[30%]">
+                <div class="relative">
+                    <img class="w-[50%]" src="/img/logo.png" />
+                </div>
+                <div class="absolute top-[18%] opacity-80 left-15">
+                    <img class="w-[90%]" src="/img/sign.png" />
+                </div>
             </div>
-            <div class="absolute top-[18%] opacity-80 left-15">
-                <img class="w-[90%]" src="/img/sign.png" />
+
+            <div class="md:w-[50%] text-center text-xl tracking-widest font-thin leading-10">
+                A Place of Style and Quality.
+                We are more than just a brand; we are your first choice for high-end leather goods, classic apparel, and
+                sophisticated home décor.
+                Each item is crafted to combine comfort, character, and craftsmanship.
             </div>
+
+            <div class=" w-[300px] flex flex-col items-center justify-center gap-2">
+                <p class="mb-5">A proudly Brand by Bishaloy</p>
+                <a target="_blank" href="https://www.bishaloy.com">
+                    <div class=" flex flex-col items-center justify-center gap-3">
+                        <div class="relative">
+                            <img class="w-[200px]" src="https://www.bishaloy.com/logo/logo-main.png" />
+                        </div>
+                        <span class="text-xl tracking-widest">www.bishaloy.com</span>
+                    </div>
+                </a>
+            </div>
+
         </div>
     </div>
 </template>
